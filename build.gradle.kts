7cdba9b81f9ed4d8ba060d755b3aa073dbb9a39a
Łukasz Kamiński <lukasz@kaminski.cx>
2022-12-05 22:27:15 +0100

    Add lombok config, add spock

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	java
+	groovy
 	id("org.springframework.boot") version "3.0.0"
 	id("io.spring.dependency-management") version "1.1.0"
 }
@@ -12,9 +13,22 @@ repositories {
 	mavenCentral()
 }
 
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.spockframework:spock-core:2.3-groovy-4.0")
+	testImplementation("org.spockframework:spock-spring:2.3-groovy-4.0")
+	testRuntimeOnly("net.bytebuddy:byte-buddy:1.12.19")
 }
 
 tasks.withType<Test> {
